063a420b76b949ffa333c00d5f4d45c1bdabfde4
RubCast <131164455+RubCast@users.noreply.github.com>
2023-11-02 22:53:16 +0800

    Holes work o7
    HOLE

--- Main.cpp
+++ Main.cpp
@@ -11,12 +11,28 @@ const float FRICTION = 1.0f;
 struct Ball {
     Vector2 position;
     float radius;
+    bool is_cue;
     Color color;
 
     float mass;
     float inverse_mass; // A variable for 1 / mass. Used in the calculation for acceleration = sum of forces / mass
     Vector2 acceleration;
     Vector2 velocity;
+};
+
+Ball Balls[5] = {
+    {400, 400, 30.0f, true, WHITE, 0.2f, 1 / Balls[0].mass,Vector2Zero(), Vector2Zero()}, 
+    {100, 100, 30.0f, false, RED, 0.2f, 1 / Balls[1].mass, Vector2Zero(), Vector2Zero()},
+    {150, 150, 30.0f, false, ORANGE, 0.2f, 1 / Balls[2].mass, Vector2Zero(), Vector2Zero()},
+    {200, 200, 30.0f, false, YELLOW, 0.2f, 1 / Balls[3].mass, Vector2Zero(), Vector2Zero()},
+    {200, 200, 30.0f, false, BLUE, 0.2f, 1 / Balls[4].mass, Vector2Zero(), Vector2Zero()}
+};
+
+Ball Holes[4] = {
+    {15, 15, 50.0f, false, BLACK, 100.0f, 0.0f, Vector2Zero(), Vector2Zero()},
+    {785, 15, 50.0f, false, BLACK, 100.0f, 0.0f, Vector2Zero(), Vector2Zero()},
+    {15, 585, 50.0f, false, BLACK, 100.0f, 0.0f, Vector2Zero(), Vector2Zero()},
+    {785, 585, 50.0f, false, BLACK, 100.0f, 0.0f, Vector2Zero(), Vector2Zero()}
 };
 
 struct Wall {
@@ -29,23 +45,14 @@ struct Wall {
     float inverse_mass;
 };
 
+Wall Walls[4] = {
+    {0, 0, 30.0f, 600.0f, RED, 0.0f, 0.0f, 100.0f, 0.0f},
+    {770, 0, 30.0f, 600.0f, RED, 0.0f, 0.0f, 100.0f, 0.0f},
+    {0, 0, 800.0f, 30.0f, RED, 0.0f, 0.0f, 100.0f, 0.0f},
+    {0, 570, 800.0f, 30.0f, RED, 0.0f, 0.0f, 100.0f, 0.0f}
+};
 
 int main() {
-    
-    Ball Balls[4] = {
-        {400, 400, 30.0f, RED, 1.5f, 1 / Balls[0].mass,Vector2Zero(), Vector2Zero()}, 
-        {100, 100, 30.0f, GREEN, 0.4f, 1 / Balls[1].mass, Vector2Zero(), Vector2Zero()},
-        {150, 150, 30.0f, BLUE, 2.0f, 1 / Balls[2].mass, Vector2Zero(), Vector2Zero()},
-        {200, 200, 30.0f, ORANGE, 7.0f, 1 / Balls[3].mass, Vector2Zero(), Vector2Zero()}
-    };
-
-    Wall Walls[4] = {
-        {0, 0, 30.0f, 600.0f, RED, 0.0f, 0.0f, 100.0f, 0.0f},
-        {770, 0, 30.0f, 600.0f, RED, 0.0f, 0.0f, 100.0f, 0.0f},
-        {0, 0, 800.0f, 30.0f, RED, 0.0f, 0.0f, 100.0f, 0.0f},
-        {0, 570, 800.0f, 30.0f, RED, 0.0f, 0.0f, 100.0f, 0.0f}
-    };
-
 
     InitWindow(WINDOW_WIDTH, WINDOW_HEIGHT, "Physics Demo");
 
@@ -79,16 +86,39 @@ int main() {
         accumulator += delta_time;
         while(accumulator >= TIMESTEP) {
             // ------ SEMI-IMPLICIT EULER INTEGRATION -------
-            for(int i = 0; i < 4; i++){
+            for(int i = 0; i < 5; i++){
             // Computes for velocity using v(t + dt) = v(t) + (a(t) * dt)
                 Balls[i].velocity = Vector2Add(Balls[i].velocity, Vector2Scale(Balls[i].acceleration, TIMESTEP));
             // Reduces the velocity by applying friction using v(n) = v(n - 1) - b / m * v(n - 1) * t
                 Balls[i].velocity = Vector2Subtract(Balls[i].velocity, Vector2Scale(Balls[i].velocity, FRICTION * Balls[i].inverse_mass * TIMESTEP));
             // Computes for change in position using x(t + dt) = x(t) + (v(t + dt) * dt)
                 Balls[i].position = Vector2Add(Balls[i].position, Vector2Scale(Balls[i].velocity, TIMESTEP));
+                
+                // Holes Collisions
+                for(int x = 0; x < 5; x++){
+                    float totalRadius = Balls[x].radius + Holes[i].radius;
+                    float distanceBetweenCircles = Vector2Distance(Balls[x].position, Holes[i].position);
 
+                    Vector2 relativeVelocity = Vector2Subtract(Balls[x].velocity, Holes[i].velocity);
+                    Vector2 collisionNorm = Vector2Subtract(Balls[x].position, Holes[i].position);
+                    float approachCheck = Vector2DotProduct(relativeVelocity, collisionNorm);
+
+                    if ((totalRadius >= distanceBetweenCircles) && (approachCheck < 0)){
+                        float impulse = -(((1 + elasticity) * Vector2DotProduct(relativeVelocity, collisionNorm))
+                        /((Vector2DotProduct(collisionNorm, collisionNorm)) * (Holes[i].inverse_mass + Balls[x].inverse_mass)));
+                        if(Balls[x].is_cue == true){
+                            Balls[x].position.x = 400;
+                            Balls[x].position.y = 400;
+                        }
+                        else{
+                            Balls[x].radius = 0.0f;
+                            Balls[x].position = Vector2Zero();
+                        }
+                    } 
+                }
+                
                 // This is for all other circles
-                for(int x = 0; x < 4; x++){
+                for(int x = 0; x < 5; x++){
                     if(x == i){
                         x++;
                     }
@@ -110,7 +140,7 @@ int main() {
                 }
                 
                 // Wall Collisions
-                for(int w = 0; w < 4; w++){
+                for(int w = 0; w < 5; w++){
                     Vector2 pointOnRect;
                     pointOnRect.x = Clamp(Balls[w].position.x, Walls[i].position.x, Walls[i].position.x + Walls[i].size.x);
                     pointOnRect.y = Clamp(Balls[w].position.y, Walls[i].position.y, Walls[i].position.y + Walls[i].size.y);
@@ -127,21 +157,21 @@ int main() {
                         Balls[w].velocity = Vector2Add(Balls[w].velocity, Vector2Scale(collisionNorm, impulse * Balls[w].inverse_mass));
                     }
                 }
-
-                // Holes Collisions
-
             }
             // Reduces the accumulater with the TIMESTEP
             accumulator -= TIMESTEP;
         }
 
         BeginDrawing();
-        ClearBackground(WHITE);
-        for(int i = 0; i < 4; i++){
+        ClearBackground(GREEN);
+        for(int i = 0; i < 5; i++){
             DrawCircleV(Balls[i].position, Balls[i].radius, Balls[i].color);
         }
         for(int i = 0; i < 4; i++){
             DrawRectangleV(Walls[i].position, Walls[i].size, Walls[i].color);
+        }
+        for(int i = 0; i < 4; i++){
+            DrawCircleV(Holes[i].position, Holes[i].radius, Holes[i].color);
         }
         EndDrawing();
     }
